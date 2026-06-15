44dbe921781bd95a033890993339df21b67eea17
Vishwanath-Qualizeal <vishwanaththepakulo@gmail.com>
2026-06-15 22:47:04 +0530

    started implementing state 1

--- state.py
+++ state.py
@@ -4,24 +4,33 @@ from langgraph.graph import StateGraph, START, END
 
 
 class State(TypedDict):
-    add : int
-    sub : int 
+    a : int
+    b : int 
     mult : int
     div : int
 
 # Define tools
 
 
-def addition(a: int, b: int):
+def addition(a: int, b: int) -> int:
     return a + b
 
-def subtraction(a: int, b: int):
+def addition_node(state:State):
+    a = state['a']
+    b = state['b']
+    result = addition(a,b)
+
+    return {
+        'add_result':result
+    }
+
+def subtraction(a: int, b: int) -> int:
     return a - b
 
-def multiply(a: int, b: int):
+def multiply(a: int, b: int) -> int:
     return a * b
 
-def divide(a: int, b: int):
+def divide(a: int, b: int) -> float:
     return a / b
 
 graph = StateGraph(State)
